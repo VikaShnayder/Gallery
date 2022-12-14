8fc08c5e7975be8a6d1030953b3817fe7ed8e74a
shnayder.1206 <shnayder.1206@gmail.com>
2022-12-14 16:29:25 +0700

    Initial commit

---
+++ app/src/main/java/com/shnayder/android/photogallery/PhotoGalleryActivity.kt
@@ -0,0 +1,10 @@
+package com.shnayder.android.photogallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
