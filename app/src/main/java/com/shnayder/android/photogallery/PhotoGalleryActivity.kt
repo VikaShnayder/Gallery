4889113f079c1a3e55931b12abef428d4652a891
shnayder.1206 <shnayder.1206@gmail.com>
2022-12-16 14:23:42 +0700

    настройка класса PhotoGalleryFragment.kt, создание макета fragment_photo_gallery.xml.

--- app/src/main/java/com/shnayder/android/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/shnayder/android/photogallery/PhotoGalleryActivity.kt
@@ -6,5 +6,15 @@ import android.os.Bundle
 class PhotoGalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_gallery)
+
+        //способ определения, размещен ли фрагмент: проверка на не null
+        //Если пакет не null, это означает, что activity восстанавливается после уничтожения системы (например, после поворота или уничтожения процесса), и все фрагменты, которые были размещены до уничтожения, были восстановлены и добавлены обратно в соответствующие контейнеры
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance()).commit()
+        }
     }
 }
+
+
