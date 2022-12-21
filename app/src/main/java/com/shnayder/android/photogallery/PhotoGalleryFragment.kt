065a465eaaf08f75465d0801ece03311dc2c722e
shnayder.1206 <shnayder.1206@gmail.com>
2022-12-21 21:10:35 +0700

    Выполнение веб-запроса в PhotoGalleryFragment.kt

--- app/src/main/java/com/shnayder/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/shnayder/android/photogallery/PhotoGalleryFragment.kt
@@ -1,6 +1,7 @@
 package com.shnayder.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,13 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.shnayder.android.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 
 class PhotoGalleryFragment : Fragment() {
@@ -17,12 +24,32 @@ class PhotoGalleryFragment : Fragment() {
     //Использование объекта Retrofit для создания экземпляра API (реализации интерфейса FlickrApi)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         //настройка и сборка экземпляра Retrofit
-        //build() - возвращает экземпляр Retrofit
-        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.flickr.com/").build()
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            //конвертер, который заставляет Retrofit десериализовать ответ в строки
+            .addConverterFactory(ScalarsConverterFactory.create())
+            //build() - возвращает экземпляр Retrofit
+            .build()
 
         //создание экземпляра интерфейса API
         val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+        // генерации объекта retrofit2.Call, представляющего собой исполняемый веб-запрос.
+        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
+
+        //выполнение веб-запроса
+        //enqueue(...) выполняет веб-запрос, находящийся в объекте Call в фоновом потоке
+        flickrHomePageRequest.enqueue(object : Callback<String> {
+            //вызывается если ответа от сервера нет
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetchphotos" , t)
+            }
+            //вызывается если ответ от сервера получен
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received:${response.body()}")
+            }
+        })
     }
 
     override fun onCreateView(
